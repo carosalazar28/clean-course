272c119dfa1bfbfe30653df7b08a18d7335a2a61
Carolina <cindy.s.r.28@hotmail.com>
2022-08-14 11:33:39 -0500

    name of variables

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,51 @@
+(() => {
+
+  // Ejemplo
+  // Archivos a evaluar - files to evaluate
+  const filesToEvaluate = [
+      { id: 1, deleteable: false },
+      { id: 2, deleteable: false },
+      { id: 3, deleteable: true },
+      { id: 4, deleteable: false },
+      { id: 5, deleteable: false },
+      { id: 7, deleteable: true },
+  ]; 
+  
+  // Archivos marcados para borrar - files to delete
+  const arhivos = filesToEvaluate.map(fileToEvaluate => fileToEvaluate.deleteable );
+
+  
+  class AbstractUser { };
+  class UserMixin { };
+  class UserImplementation { };
+  interface IUser { };
+
+  // Mejor
+  class User { };
+  interface User { };
+
+
+  // Todo: Tarea    
+  // día de hoy - today
+  const currentDate = new Date();
+  
+  // días transcurridos - elapsed time in days
+  const elapsedTimeInDays: number = 23;
+  
+  // número de archivos en un directorio - number of files in directory
+  const numberOfFilesInDirectory = 33;
+  
+  // primer nombre - first name
+  const firstName = 'Fernando';
+  
+  // primer apellido - last name
+  const lastName = 'Herrera';
+
+  // días desde la última modificación - days since modification
+  const daysSinceLastModification = 12;
+  
+  // cantidad máxima de clases por estudiante - max classes per student
+  const maxClassesPerStudent = 6;
+
+
+})();
